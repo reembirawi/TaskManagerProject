137ff6ab401698a7ff08a24d185ab99688e19849
Reem <reembirawi@gmail.com>
2026-06-25 14:30:07 +0300

    Implement title length validation in TaskSerializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -5,6 +5,14 @@ from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                "Title must be at least 3 characters long."
+            )
+
+        return value
+
     class Meta:
         model = Task
         fields = ["id", "title", "description", "status", "user"]
